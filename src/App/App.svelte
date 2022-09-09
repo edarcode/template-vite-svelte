4b69df9039bbf8c3f42851e88f921014bcd0751c
edarcode <edarcode@gmail.com>
2022-09-08 21:00:24 -0500

    init template

---
+++ src/App/App.svelte
@@ -0,0 +1,9 @@
+<script>
+</script>
+
+<main>
+	<h1>Hola mundo</h1>
+</main>
+
+<style>
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-import "./app.css";
-import App from "./App.svelte";
+import App from "./App/App.svelte";
+import "./index.css";
 
 const app = new App({
 	target: document.querySelector("#app")
